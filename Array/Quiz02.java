8e2712a5e772126124e1db81518d15f89b3d261c
홍유리 <116433637+dbfl720@users.noreply.github.com>
2023-01-16 00:26:48 +0900

    Review Algorithm that i learned before.
    Review Algorithm that i learned before.

---
+++ Array/Quiz02.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Quiz02 {
+
+	public static void main(String[] args) {
+		// 배열 응용하기
+
+//		1. 배열 값 변경
+//		index와 값을 차례로 입력 받아서, 아래 배열에 해당하는 index에 값을 바꾸고 출력 하세요.
+		
+		
+	}
+
+}
